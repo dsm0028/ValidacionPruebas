f33aa4bbb37e2135f65816df87f83a5bca9b903a
dsm0028 <sierramecanica@hotmail.com>
2018-09-19 21:00:38 +0200

    Primera cosa

---
+++ KnowOpinion/LibClases/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibClases
+{
+	class Usuario
+	{
+
+		private string contrasena;
+
+		Usuario(string _cuenta)
+		{
+			this.cuenta = _cuenta;
+			this.contrasena = "";
+
+		}
+
+		private string cuenta;
+		public string Cuenta
+		{
+			get { return cuenta; }
+			set { this.cuenta = value; }
+		}
+	}
+}
