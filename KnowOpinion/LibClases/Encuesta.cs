ddf045b7d5281e66f3dea10736db3606301bd069
AlissonRomero <arc0067@alu.ubu.es>
2018-10-02 20:07:25 +0200

    Commit2: Creacion de encuesta y test de encuesta

---
+++ KnowOpinion/LibClases/Encuesta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibClases
+{
+    public class Encuesta
+    {
+        private string num_encuesta;
+        public string Num_encuesta
+        {
+            get { return num_encuesta; }
+            set { this.num_encuesta = value; }
+        }
+
+        private int valoracion;
+        public int Valoracion
+        {
+            get { return valoracion; }
+            set { if (value >= 1 && value <= 4) 
+                {
+                    this.valoracion = value;
+                }
+            }
+        }
+        private string descripcion;
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { this.descripcion = value; }
+        }
+
+
+    }
+}
