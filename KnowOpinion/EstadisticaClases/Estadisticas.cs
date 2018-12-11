eb3b5254660177678528b42ab42f96d531391861
dsm0028 <sierramecanica@hotmail.com>
2018-12-11 17:06:16 +0100

    Sprint 2 refactorizado

--- KnowOpinion/EstadisticaClases/Estadisticas.cs
+++ KnowOpinion/EstadisticaClases/Estadisticas.cs
@@ -57,7 +57,10 @@ namespace EstadisticaClases
                     numeroRespuestas++;
                 }
             }
-
+            if(numeroRespuestas == 0.0)
+            {
+                return 0.0;
+            }
             return sumatorio / numeroRespuestas;
 
         }
@@ -75,12 +78,17 @@ namespace EstadisticaClases
                 }
             }
 
+            if(vals.Count == 0)
+            {
+                return 0.0;
+            }
+
             vals.Sort();
 
             int midpoint = vals.Count();
             if(midpoint % 2 != 0)
             {
-                return vals[(midpoint / 2) - 1];
+                return vals[(midpoint / 2)];
             } else
             {
                 double half = (midpoint / 2) - 1;
@@ -103,6 +111,11 @@ namespace EstadisticaClases
                 }
             }
 
+            if (vals.Count <= 1)
+            {
+                return 0.0;
+            }
+
             double med = media();
             double varianza = 0.0;
             
@@ -113,6 +126,107 @@ namespace EstadisticaClases
 
             return Math.Sqrt(varianza / (vals.Count - 1));
 
+        }
+
+        private List<Respuesta> recolectarTodas()
+        {
+            List<Respuesta> l = new List<Respuesta>();
+            foreach(Encuesta e in bd.ObtenerTodas())
+            {
+                foreach(Respuesta r in e.ObtenerRespuestas())
+                {
+                    l.Add(r);
+                }
+            }
+            return l;
+        }
+
+        public DataTable respuestasPorSemanas()
+        {
+            List<Respuesta> res = recolectarTodas();
+
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Dia de la semana", typeof(string));
+            tabla.Columns.Add("Numero", typeof(int));
+
+            for(int i = 10; i < 17; i++)
+            {
+                string diaSemana = new DateTime(2018, 12, i).ToString("dddd");
+                tabla.Rows.Add(diaSemana, 0);
+            }
+
+            foreach(Respuesta r in res)
+            {
+                DateTime f = r.Fecha;
+                int currval =(int) tabla.Rows[(int)f.DayOfWeek]["Numero"];
+                tabla.Rows[(int)f.DayOfWeek]["Numero"] = currval + 1;
+            }
+
+            return tabla;
+        }
+
+        public DataTable respuestasPorMeses()
+        {
+            List<Respuesta> res = recolectarTodas();
+
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Mes", typeof(string));
+            tabla.Columns.Add("Numero", typeof(int));
+
+            for (int i = 1; i < 13; i++)
+            {
+                string mes = new DateTime(2018, i, 1).ToString("MMMM");
+                tabla.Rows.Add(mes, 0);
+            }
+
+            foreach (Respuesta r in res)
+            {
+                DateTime f = r.Fecha;
+                int currval = (int)tabla.Rows[f.Month - 1]["Numero"];
+                tabla.Rows[f.Month - 1]["Numero"] = currval + 1;
+            }
+
+            return tabla;
+
+        }
+
+        public DataTable respuestasPorAnios()
+        {
+            List<Respuesta> res = recolectarTodas();
+
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Año", typeof(int));
+            tabla.Columns.Add("Numero", typeof(int));
+
+            int anoMenor = DateTime.Now.Year;
+            int anoMayor = 0;
+            
+            foreach(Respuesta r in res)
+            {
+                if(r.Fecha.Year < anoMenor)
+                {
+                    anoMenor = r.Fecha.Year;
+                }
+                if(r.Fecha.Year > anoMayor)
+                {
+                    anoMayor = r.Fecha.Year;
+                }
+
+            }
+
+            for(int i = anoMenor; i <= anoMayor; i++)
+            {
+                tabla.Rows.Add(i, 0);
+            }
+
+            foreach (Respuesta r in res)
+            {
+                DateTime f = r.Fecha;
+                int currval = (int)tabla.Rows[f.Year - anoMenor]["Numero"];
+                tabla.Rows[f.Year - anoMenor]["Numero"] = currval + 1;
+            }
+
+            return tabla;
         }
 
         public DataTable rankingEncuestasPorRespuestas()
@@ -134,6 +248,61 @@ namespace EstadisticaClases
 
         }
 
+        public DataTable mediaPorEncuesta()
+        {
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Titulo de la encuesta", typeof(string));
+            tabla.Columns.Add("Media", typeof(double));
+
+            List<Encuesta> lista = bd.ObtenerTodas();
+            foreach (Encuesta e in lista)
+            {
+                double sumatorio = 0.0;
+                double numeroRespuestas = 0.0;
+                foreach (Respuesta r in e.ObtenerRespuestas())
+                {
+                    sumatorio += r.Valoracion;
+                    numeroRespuestas++;
+                }
+
+                if(numeroRespuestas == 0)
+                {
+                    tabla.Rows.Add(e.Titulo, 0.0);
+                    continue;
+                }
+
+                tabla.Rows.Add(e.Titulo, sumatorio / numeroRespuestas);
+
+            }
+
+            DataView dv = tabla.DefaultView;
+            dv.Sort = "Media DESC";
+            return dv.ToTable();
+        }
+
+        public DataTable respuestasPorHoras()
+        {
+            List<Respuesta> res = recolectarTodas();
+
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Franja horaria", typeof(int));
+            tabla.Columns.Add("Numero", typeof(int));
+
+            for (int i = 0; i < 25; i++)
+            {
+                tabla.Rows.Add(i, 0);
+            }
+
+            foreach (Respuesta r in res)
+            {
+                DateTime f = r.Fecha;
+                int currval = (int)tabla.Rows[f.Hour]["Numero"];
+                tabla.Rows[f.Hour]["Numero"] = currval + 1;
+            }
+
+            return tabla;
+        }
+
         public DataTable rankingEncuestasPorValoracion()
         {
             List<Encuesta> lista = bd.ObtenerTodas();
@@ -151,6 +320,129 @@ namespace EstadisticaClases
             dv.Sort = "Nota Maxima DESC";
             return dv.ToTable();
 
+        }
+
+        public DataTable desvEstPorEncuesta()
+        {
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Titulo de la encuesta", typeof(string));
+            tabla.Columns.Add("Desviacion", typeof(double));
+
+            List<Encuesta> lista = bd.ObtenerTodas();
+            foreach(Encuesta e in lista)
+            {
+                List<int> vals = new List<int>();
+                double sumat = 0.0;
+                foreach (Respuesta r in e.ObtenerRespuestas())
+                {
+                    vals.Add(r.Valoracion);
+                    sumat += r.Valoracion;
+                }
+                if (vals.Count <= 1)
+                {
+                    tabla.Rows.Add(e.Titulo, 0.0);
+                    continue;
+                }
+
+                double med = sumat / vals.Count;
+                double varianza = 0.0;
+
+                foreach (int i in vals)
+                {
+                    varianza += Math.Pow(Convert.ToDouble(i) - med, 2);
+                }
+
+                tabla.Rows.Add(e.Titulo, Math.Sqrt(varianza / (vals.Count - 1)));
+            }
+
+            DataView dv = tabla.DefaultView;
+            dv.Sort = "Desviacion DESC";
+            return dv.ToTable();
+        }
+
+        public DataTable medianaPorEncuesta()
+        {
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Titulo de la encuesta", typeof(string));
+            tabla.Columns.Add("Mediana", typeof(double));
+
+            List<Encuesta> lista = bd.ObtenerTodas();
+            foreach(Encuesta e in lista){
+                List<int> vals = new List<int>();
+                foreach(Respuesta r in e.ObtenerRespuestas())
+                {
+                    vals.Add(r.Valoracion);
+                }
+
+                if (vals.Count == 0)
+                {
+                    tabla.Rows.Add(e.Titulo, 0.0);
+                    continue;
+                }
+
+                vals.Sort();
+
+                int midpoint = vals.Count();
+                if (midpoint % 2 != 0)
+                {
+                    tabla.Rows.Add(e.Titulo, vals[(midpoint / 2)]);
+                }
+                else
+                {
+                    double half = (midpoint / 2) - 1;
+                    double t = vals[(int)Math.Round(half)] + vals[(int)Math.Round(half) + 1];
+                    tabla.Rows.Add(e.Titulo, t / 2);
+                }
+
+            }
+
+            DataView dv = tabla.DefaultView;
+            dv.Sort = "Mediana DESC";
+            return dv.ToTable();
+        }
+
+        public DataTable numRespRangosPorEncuesta(int v)
+        {
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Titulo", typeof(string));
+            tabla.Columns.Add("Menor valoracion", typeof(int));
+            tabla.Columns.Add("Mayor valoracion", typeof(int));
+            Encuesta e = bd.GetEncuestaById(v);
+            int min = 99;
+            int max = -1;
+
+            foreach(Respuesta r in e.ObtenerRespuestas())
+            {
+                if(r.Valoracion < min)
+                {
+                    min = r.Valoracion;
+                }
+
+                if (r.Valoracion > max)
+                {
+                    max = r.Valoracion;
+                }
+            }
+
+            tabla.Rows.Add(e.Titulo, min, max);
+            return tabla;
+        }
+
+        public DataTable numRespRangos()
+        {
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Titulo", typeof(string));
+            tabla.Columns.Add("Menor valoracion", typeof(int));
+            tabla.Columns.Add("Mayor valoracion", typeof(int));
+            List<Encuesta> lista = bd.ObtenerTodas();
+
+            foreach(Encuesta e in lista)
+            {
+                DataRow fila = numRespRangosPorEncuesta(e.Id).Rows[0];
+                tabla.Rows.Add(fila[0], fila[1], fila[2]);
+            }
+
+            return tabla;
         }
     }
 }
--- KnowOpinion/EstadisticaTests/TestEstadisticas.cs
+++ KnowOpinion/EstadisticaTests/TestEstadisticas.cs
@@ -85,5 +85,134 @@ namespace EstadisticaTests
             Assert.AreEqual("Encuesta de prueba G", encuestaG[0]);
             Assert.AreEqual(0, encuestaG[1]);
         }
+
+        [TestMethod]
+        public void TestRespuestasPorAnios()
+        {
+            DataTable t = stats.respuestasPorAnios();
+
+            DataRow yr2k = t.Rows[0];
+
+            Assert.AreEqual(2000, yr2k[0]);
+            Assert.AreEqual(18, yr2k[1]);
+
+        }
+
+        [TestMethod]
+        public void TestRespuestasPorMeses()
+        {
+            DataTable t = stats.respuestasPorMeses();
+
+            DataRow jan = t.Rows[0];
+
+            Assert.AreEqual("enero", jan[0]);
+            Assert.AreEqual(18, jan[1]);
+        }
+
+        [TestMethod]
+        public void TestRespuestasPorSemanas()
+        {
+            DataTable t = stats.respuestasPorSemanas();
+
+            DataRow mon = t.Rows[0];
+            DataRow thu = t.Rows[3];
+
+            Assert.AreEqual("lunes", mon[0]);
+            Assert.AreEqual(3, mon[1]);
+
+            Assert.AreEqual("jueves", thu[0]);
+            Assert.AreEqual(2, thu[1]);
+        }
+
+        [TestMethod]
+        public void TestRespuestasPorHoras()
+        {
+            DataTable t = stats.respuestasPorHoras();
+
+            DataRow morning = t.Rows[10]; //10:00
+            DataRow lateNight = t.Rows[0]; //0:00
+
+            Assert.AreEqual(10, morning[0]);
+            Assert.AreEqual(1, morning[1]);
+
+            Assert.AreEqual(0, lateNight[0]);
+            Assert.AreEqual(1, lateNight[1]);
+        }
+
+        [TestMethod]
+        public void TestMediaPorEncuesta()
+        {
+            DataTable t = stats.mediaPorEncuesta();
+
+            DataRow e1 = t.Rows[0];
+            DataRow e7 = t.Rows[6]; //no tiene respuestas
+
+            Assert.AreEqual("Encuesta de prueba A", e1[0]);
+            Assert.AreEqual(3.66, (double)e1[1], 0.01);
+
+            Assert.AreEqual("Encuesta de prueba G", e7[0]);
+            Assert.AreEqual(0.0, e7[1]);
+        }
+
+        [TestMethod]
+        public void TestMedianaPorEncuesta()
+        {
+            DataTable t = stats.medianaPorEncuesta();
+
+            DataRow e1 = t.Rows[0];
+            DataRow e7 = t.Rows[6]; //no tiene respuestas
+
+            Assert.AreEqual("Encuesta de prueba A", e1[0]);
+            Assert.AreEqual(4.0, e1[1]);
+
+            Assert.AreEqual("Encuesta de prueba G", e7[0]);
+            Assert.AreEqual(0.0, e7[1]);
+        }
+
+        [TestMethod]
+        public void TestDesvEstPorEncuesta()
+        {
+            DataTable t = stats.desvEstPorEncuesta();
+
+            DataRow e2 = t.Rows[0];
+            DataRow e7 = t.Rows[6]; //no tiene respuestas
+
+            Assert.AreEqual("Encuesta de prueba E", e2[0]);
+            Assert.AreEqual(1.73, (double)e2[1], 0.01);
+
+            Assert.AreEqual("Encuesta de prueba G", e7[0]);
+            Assert.AreEqual(0.0, e7[1]);
+        }
+
+        [TestMethod]
+        public void TestNumRespRangosPorEncuesta()
+        {
+            DataRow encuestaA = stats.numRespRangosPorEncuesta(1).Rows[0];
+            DataRow encuestaB = stats.numRespRangosPorEncuesta(2).Rows[0];
+
+            Assert.AreEqual("Encuesta de prueba A", encuestaA[0]);
+            Assert.AreEqual(3, encuestaA[1]);
+            Assert.AreEqual(4, encuestaA[2]);
+
+            Assert.AreEqual("Encuesta de prueba B", encuestaB[0]);
+            Assert.AreEqual(2, encuestaB[1]);
+            Assert.AreEqual(4, encuestaB[2]);
+        }
+
+        [TestMethod]
+        public void TestNumRespRangos()
+        {
+            DataTable t = stats.numRespRangos();
+            DataRow encuestaA = t.Rows[0];
+            DataRow encuestaB = t.Rows[1];
+
+            Assert.AreEqual("Encuesta de prueba A", encuestaA[0]);
+            Assert.AreEqual(3, encuestaA[1]);
+            Assert.AreEqual(4, encuestaA[2]);
+
+            Assert.AreEqual("Encuesta de prueba B", encuestaB[0]);
+            Assert.AreEqual(2, encuestaB[1]);
+            Assert.AreEqual(4, encuestaB[2]);
+        }
     }
 }
